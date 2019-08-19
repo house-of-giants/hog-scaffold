f98064b61f595247404ab64607bc1a27d3821515
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-08-19 08:49:11 -0600

    Remove temp browserstack script.

--- header.php
+++ header.php
@@ -11,9 +11,6 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="theme-color" content="#d23226" />
-		<script id="__bs_script__">//<![CDATA[
-				document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
-		//]]></script>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
