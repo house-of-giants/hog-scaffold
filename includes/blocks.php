c0e08d4f657fecac36f0b697788306d542ce05a4
Dominic Magnifico <dom@houseofgiants.com>
2021-06-09 12:52:24 -0600

    Refactor the block factory method of registering blocks in favor of a much more modular approach.

--- includes/blocks.php
+++ includes/blocks.php
@@ -7,8 +7,7 @@
 
 namespace HoGScaffold\Blocks;
 
-// Require custom block classes.
-require_once HOG_SCAFFOLD_INC . 'blocks/block-factory.php';
+use HoGScaffold\Blocks\Example;
 
 /**
  * Set up blocks
@@ -20,38 +19,46 @@ function setup() {
 		return __NAMESPACE__ . "\\$function";
 	};
 
-	add_action( 'init', $n( 'init_blocks' ) );
-
 	add_action( 'enqueue_block_editor_assets', $n( 'blocks_editor_scripts' ) );
 
 	add_filter( 'block_categories', $n( 'blocks_categories' ), 10, 2 );
+
+	add_action( 'init', $n( 'register_theme_blocks' ) );
+
+	add_action( 'init', $n( 'block_patterns_and_categories' ) );
 }
 
 /**
- * Register blocks.
+ * Add in blocks that are registered in this theme
  *
  * @return void
  */
-function init_blocks() {
-	// BLOCK_NAME::get_new();
+function register_theme_blocks() {
+	// Filter the plugins URL to allow us to have blocks in themes with linked assets. i.e editorScripts
+	add_filter( 'plugins_url', __NAMESPACE__ . '\filter_plugins_url', 10, 2 );
+
+	// Require custom blocks.
+	require_once HOG_SCAFFOLD_BLOCK_DIR . '/example-block/register.php';
+
+	// Call block register functions for each block.
+	Example\register();
+
+	// Remove the filter after we register the blocks
+	remove_filter( 'plugins_url', __NAMESPACE__ . '\filter_plugins_url', 10, 2 );
 }
 
 /**
- * Enqueue shared frontend and editor JavaScript for blocks.
+ * Filter the plugins_url to allow us to use assets from theme.
  *
- * @return void
+ * @param string $url  The plugins url
+ * @param string $path The path to the asset.
+ *
+ * @return string The overridden url to the block asset.
  */
-function blocks_scripts() {
-
-	wp_enqueue_script(
-		'blocks',
-		HOG_SCAFFOLD_TEMPLATE_URL . '/dist/js/blocks.min.js',
-		[],
-		HOG_SCAFFOLD_VERSION,
-		true
-	);
+function filter_plugins_url( $url, $path ) {
+	$file = preg_replace( '/\.\.\//', '', $path );
+	return trailingslashit( get_stylesheet_directory_uri() ) . $file;
 }
-
 
 /**
  * Enqueue editor-only JavaScript/CSS for blocks.
@@ -69,12 +76,12 @@ function blocks_editor_scripts() {
 	);
 
 	$localized_variables = array(
-		'ajax_url'  => admin_url( 'admin-ajax.php' ),
+		'ajax_url' => admin_url( 'admin-ajax.php' ),
 	);
 	wp_localize_script( 'blocks-editor', 'localizedVariables', $localized_variables );
 
 	wp_enqueue_style(
-		'editor-style',
+		'shared-style',
 		HOG_SCAFFOLD_TEMPLATE_URL . '/dist/css/shared-style.css',
 		[],
 		HOG_SCAFFOLD_VERSION
@@ -84,6 +91,12 @@ function blocks_editor_scripts() {
 		wp_enqueue_style(
 			'admin-style',
 			HOG_SCAFFOLD_TEMPLATE_URL . '/dist/css/admin-style.css',
+			[],
+			HOG_SCAFFOLD_VERSION
+		);
+		wp_enqueue_style(
+			'editor-style',
+			HOG_SCAFFOLD_TEMPLATE_URL . '/dist/css/editor-style.css',
 			[],
 			HOG_SCAFFOLD_VERSION
 		);
@@ -108,9 +121,45 @@ function blocks_categories( $categories, $post ) {
 		$categories,
 		array(
 			array(
-				'slug'  => 'HoGScaffold-blocks',
-				'title' => __( 'Custom Blocks', 'HoGScaffold' ),
+				'slug'  => 'hog-scaffold-blocks',
+				'title' => __( 'Custom Blocks', 'hog' ),
 			),
 		)
 	);
+}
+
+/**
+ * Manage block patterns and block pattern categories
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-patterns/
+ *
+ * @return void
+ */
+function block_patterns_and_categories() {
+	/*
+		## Examples
+
+		// Register block pattern
+		register_block_pattern(
+			'hog/block-pattern',
+			array(
+						'title'       => __( 'Two buttons', 'hog' ),
+						'description' => _x( 'Two horizontal buttons, the left button is filled in, and the right button is outlined.', 'Block pattern description', 'wpdocs-my-plugin' ),
+						'content'     => "<!-- wp:buttons {\"align\":\"center\"} -->\n<div class=\"wp-block-buttons aligncenter\"><!-- wp:button {\"backgroundColor\":\"very-dark-gray\",\"borderRadius\":0} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-background has-very-dark-gray-background-color no-border-radius\">" . esc_html__( 'Button One', 'wpdocs-my-plugin' ) . "</a></div>\n<!-- /wp:button -->\n\n<!-- wp:button {\"textColor\":\"very-dark-gray\",\"borderRadius\":0,\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-text-color has-very-dark-gray-color no-border-radius\">" . esc_html__( 'Button Two', 'wpdocs-my-plugin' ) . "</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons -->",
+				)
+		);
+
+		// Unregister a block pattern
+		unregister_block_pattern( 'hog/block-pattern' );
+
+		// Register a block pattern category
+		register_block_pattern_category(
+			'client-name',
+				array( 'label' => __( 'Client Name', 'hog' ) )
+		);
+
+		// Unregister a block pattern category
+		unregister_block_pattern('client-name')
+
+	*/
 }
