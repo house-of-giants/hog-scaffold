0bf00d4c306968f7851032f149d0d9d7cfbc84fd
Dominic Magnifico <dom@houseofgiants.com>
2021-06-09 12:52:53 -0600

    Include block registration example using a more modular approach.

---
+++ includes/blocks/example-block/markup.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Example block markup
+ *
+ * @package HoGScaffold\Blocks\Example
+ *
+ * @var array $args {
+ *     $args is provided by get_template_call.
+ *
+ *     @type array $attributes Block attributes.
+ *     @type array $content    Block content.
+ *     @type array $block      Block instance.
+ * }
+ */
+
+// Set defaults.
+$args = wp_parse_args(
+	$args,
+	[
+		'attributes' => [
+			'customTitle' => __( 'Custom title default', 'hog' ),
+		],
+		'class_name' => 'wp-block-example',
+	]
+);
+
+?>
+<div class="<?php echo esc_attr( $args['class_name'] ); ?>">
+	<h2 class="wp-block-example-title">
+		<?php echo wp_kses_post( $args['attributes']['customTitle'] ); ?>
+	</h2>
+	Some stuff here I guess
+</div>
