0fcc99c113da4c52850a6536c8759171e3cf91f4
Dominic Magnifico <dom@houseofgiants.com>
2021-06-11 14:20:41 -0600

    Remove unnecssary externals.

--- config/externals.js
+++
@@ -1,7 +0,0 @@
-/**
- * Define externals to load components through the wp global.
- */
-module.exports = {
-	jquery: 'jQuery', // import $ from 'jquery' // Use the WordPress version after enqueuing it.
-	lodash: 'lodash', // Lodash is there in Gutenberg.
-}
