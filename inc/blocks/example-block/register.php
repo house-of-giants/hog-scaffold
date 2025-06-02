7698e4291f98ab376769eae2133460d992922a18
Dominic Magnifico <dominicmagnifico@gmail.com>
2025-06-02 13:19:45 -0600

    fix: update block template path reference

--- inc/blocks/example-block/register.php
+++ inc/blocks/example-block/register.php
@@ -10,17 +10,18 @@ namespace HoGScaffold\Blocks\Example;
 /**
  * Register the block
  */
-function register() {
-	$n = function( $function ) {
-		return __NAMESPACE__ . "\\$function";
-	};
-	// Register the block.
-	register_block_type_from_metadata(
-		HOG_SCAFFOLD_BLOCK_DIR . '/example-block', // this is the directory where the block.json is found.
-		[
-			'render_callback' => $n( 'render_block_callback' ),
-		]
-	);
+function register()
+{
+  $n = function ($function) {
+    return __NAMESPACE__ . "\\$function";
+  };
+  // Register the block.
+  register_block_type_from_metadata(
+    HOG_SCAFFOLD_BLOCK_DIR . '/example-block', // this is the directory where the block.json is found.
+    [
+      'render_callback' => $n('render_block_callback'),
+    ]
+  );
 }
 
 /**
@@ -32,18 +33,19 @@ function register() {
  *
  * @return string The rendered block markup.
  */
-function render_block_callback( $attributes, $content, $block ) {
-	ob_start();
-	get_template_part(
-		'includes/blocks/example-block/markup',
-		null,
-		[
-			'class_name' => 'wp-block-example',
-			'attributes' => $attributes,
-			'content'    => $content,
-			'block'      => $block,
-		]
-	);
+function render_block_callback($attributes, $content, $block)
+{
+  ob_start();
+  get_template_part(
+    'inc/blocks/example-block/markup',
+    null,
+    [
+      'class_name' => 'wp-block-example',
+      'attributes' => $attributes,
+      'content' => $content,
+      'block' => $block,
+    ]
+  );
 
-	return ob_get_clean();
+  return ob_get_clean();
 }
